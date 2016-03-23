bd0d4a987ba082c8787c3f03ae228d81352dabcf
John <John@10.200.6.175>
2016-03-23 09:59:49 -0500

    changed the colors of orbitiong planets

--- src/nbody/Body.java
+++ src/nbody/Body.java
@@ -1,6 +1,7 @@
 package nbody;
 
 import edu.princeton.cs.StdDraw;
+import java.awt.Color;
 
 /******************************************************************************
  *  Compilation:  javac Body.java
@@ -39,8 +40,21 @@ public class Body {
     } // forceFrom( Body )
 
     public void draw() {
-        StdDraw.setPenRadius(0.025);
+        StdDraw.setPenRadius(0.015);
+//        int red = (int) Math.round(Math.random() * 255);
+//        int green = (int) Math.round(Math.random() * 255);
+//        int blue = (int) Math.round(Math.random() * 255);
+//        Color color = new Color(red, green, blue);
+//        StdDraw.setPenColor(color);
         StdDraw.point(r.cartesian(0), r.cartesian(1));
+        StdDraw.setPenColor(Color.BLACK);
+        for (int i = 0; i < 200; i++) {
+            double x0 = Math.random() * 2 - 2;
+            double y0 = Math.random() * 2 - 2;
+            double r0 = Math.random();
+            StdDraw.point(x0, y0);
+        } // for
+        StdDraw.setPenRadius(.001);
     } // draw()
 
     // this method is only needed if you want to change the size of the bodies
